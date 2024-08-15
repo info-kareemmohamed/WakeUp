6240383df32a3c38acb3e46e445ca919e4d0fd1d
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-08-15 04:37:50 +0300

    domain : Cancel alarm use case

---
+++ app/src/main/java/com/example/alarmclock/domain/use_case/CancelAlarmUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.alarmclock.domain.use_case
+
+import android.app.AlarmManager
+import android.content.Context
+import com.example.alarmclock.common.getPendingIntent
+import com.example.alarmclock.data.model.Alarm
+
+class CancelAlarmUseCase {
+    operator fun invoke(alarm: Alarm, context: Context) {
+        val alarmManager: AlarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarm.getDaysOfWeek().forEach{ day ->
+            alarmManager.cancel(alarm.getPendingIntent(context,day))
+        }
+    }
+}
