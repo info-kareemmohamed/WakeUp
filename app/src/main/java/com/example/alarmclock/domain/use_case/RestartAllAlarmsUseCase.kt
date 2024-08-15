3fb9e7d05e5124ac25b12c72b5322f6f369d3373
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-08-15 04:37:14 +0300

    domain : restart all alarms use case

---
+++ app/src/main/java/com/example/alarmclock/domain/use_case/RestartAllAlarmsUseCase.kt
@@ -0,0 +1,22 @@
+package com.example.alarmclock.domain.use_case
+
+import android.content.Context
+import com.example.alarmclock.data.local.alarm.dao.AlarmDao
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class RestartAllAlarmsUseCase @Inject constructor(
+    private val alarmDao: AlarmDao,
+    private val schedulerAlarmUseCase: SchedulerAlarmUseCase
+) {
+    operator fun invoke(context: Context) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val activeAlarms = alarmDao.getActiveAlarm()
+            activeAlarms.forEach { alarm ->
+                schedulerAlarmUseCase(alarm, context)
+            }
+        }
+    }
+}
