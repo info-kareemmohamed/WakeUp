be73f1e9c3253a4c721090ed8cc73f8a4d3e0394
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-08-16 09:03:46 +0300

    domain :  Snooze alarm use case

---
+++ app/src/main/java/com/example/alarmclock/domain/use_case/SnoozeAlarmUseCase.kt
@@ -0,0 +1,33 @@
+package com.example.alarmclock.domain.use_case
+
+import android.app.AlarmManager
+import android.content.Context
+import android.os.Build
+import com.example.alarmclock.common.core.Constant.SNOOZE_TIME
+import com.example.alarmclock.common.getPendingIntent
+import com.example.alarmclock.data.model.Alarm
+import java.util.Calendar
+import javax.inject.Inject
+
+
+class SnoozeAlarmUseCase @Inject constructor() {
+    operator fun invoke(context: Context, alarm: Alarm) {
+        val calendar = Calendar.getInstance().apply { add(Calendar.MINUTE, SNOOZE_TIME) }
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val pendingIntent = alarm.getPendingIntent(context, calendar.get(Calendar.DAY_OF_WEEK))
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            alarmManager.setExactAndAllowWhileIdle(
+                AlarmManager.RTC_WAKEUP,
+                calendar.timeInMillis,
+                pendingIntent
+            )
+        } else {
+            alarmManager.setRepeating(
+                AlarmManager.RTC_WAKEUP,
+                calendar.timeInMillis,
+                AlarmManager.INTERVAL_DAY,
+                pendingIntent
+            )
+        }
+    }
+}
