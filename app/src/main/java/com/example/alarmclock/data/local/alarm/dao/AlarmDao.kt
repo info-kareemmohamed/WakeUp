5c1c87bfd1c46025399ddbeb66b5494a5dde783b
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-08-17 03:48:04 +0300

    data: Alarm Dao in Insert  replace

--- app/src/main/java/com/example/alarmclock/data/local/alarm/dao/AlarmDao.kt
+++ app/src/main/java/com/example/alarmclock/data/local/alarm/dao/AlarmDao.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.example.alarmclock.data.model.Alarm
@@ -11,7 +12,7 @@ import com.example.alarmclock.data.model.Alarm
 
 @Dao
 interface AlarmDao {
-    @Insert
+    @Insert (onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAlarm(alarm: Alarm):Long
 
     @Delete
