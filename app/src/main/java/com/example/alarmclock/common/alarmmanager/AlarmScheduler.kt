061a9d43c2d73dca443694ddba84aecb2d3318cd
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-08-17 03:45:43 +0300

    delete  Alarm Scheduler class

--- app/src/main/java/com/example/alarmclock/common/alarmmanager/AlarmScheduler.kt
+++
@@ -1,8 +0,0 @@
-package com.example.alarmclock.common.alarmmanager
-
-import com.example.alarmclock.data.model.Alarm
-
-interface AlarmScheduler {
-    fun scheduler(item: Alarm?)
-    fun cancel(item: Alarm)
-}
